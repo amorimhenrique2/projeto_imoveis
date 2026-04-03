952450615140b5044e71aeb342036b5e69df7224
AMORIMHENRIQUE2 <amorimhenrique@id.uff.br>
2026-04-03 17:33:00 -0300

    analie descritiva mais grafico

--- projeto_imoveis.R
+++ projeto_imoveis.R
@@ -15,3 +15,19 @@ library(credentials)
 base =read.csv("imoveis.csv",header=T, sep=";") 
 attach(base) 
 str(base)
+
+# Análises descritivas - variáveis qualitativas 
+
+
+prop.table(table(preco)) 
+prop.table(table(idade)) 
+prop.table(table(metragem)) 
+prop.table(table(itens_embutidos)) 
+prop.table(table(imposto_anual)) 
+
+
+hist(base$preco, main = "Histograma dos Preços dos Imóveis",
+     xlab = 'metragem',
+     ylab = 'imposto_abual',
+     col = "lightblue",
+     border = "black")
